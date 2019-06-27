d793156f626bd20160fc0343206b9054c0ee8176
Jakub Sokołowski <jakub@status.im>
2019-06-27 15:34:34 -0400

    manage cursor based on current peer position
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- keys.go
+++ keys.go
@@ -11,37 +11,12 @@ type Binding struct {
 }
 
 func (vc *ViewController) CursorUp(g *gocui.Gui, v *gocui.View) error {
-	return MoveCursor(-1, vc, g, v)
+	vc.State.SetCurrent(vc.State.GetState().Current - 1)
+	return nil
 }
 
 func (vc *ViewController) CursorDown(g *gocui.Gui, v *gocui.View) error {
-	return MoveCursor(1, vc, g, v)
-}
-
-func MoveCursor(mod int, vc *ViewController, g *gocui.Gui, v *gocui.View) error {
-	if v == nil {
-		return nil
-	}
-	cx, cy := v.Cursor()
-	// get peers
-	ps := vc.State.GetState()
-	peers := ps.Peers
-	// Don't go beyond available list of peers
-	if cy+mod >= len(peers) || cy+mod < 0 {
-		return nil
-	}
-	// update currently selected peer in the list
-	vc.State.SetCurrent(cy + mod)
-
-	if err := v.SetCursor(cx, cy+mod); err != nil {
-		if mod == -1 {
-			return nil
-		}
-		ox, oy := v.Origin()
-		if err := v.SetOrigin(ox, oy+mod); err != nil {
-			return err
-		}
-	}
+	vc.State.SetCurrent(vc.State.GetState().Current + 1)
 	return nil
 }
 
