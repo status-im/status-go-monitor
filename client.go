01e1e0605fc9c472db05c819ae4274e9d8c06d5c
Jakub Sokołowski <jakub@status.im>
2019-07-01 16:35:44 -0400

    add a call to admin_nodeInfo before launching gui
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- client.go
+++ client.go
@@ -16,6 +16,15 @@ func newClient(url string) (*StatusGoClient, error) {
 	return &StatusGoClient{rpcClient}, nil
 }
 
+func (c *StatusGoClient) nodeInfo() (*NodeInfo, error) {
+	var info NodeInfo
+	err := c.rpcClient.Call(&info, "admin_nodeInfo")
+	if err != nil {
+		return nil, err
+	}
+	return &info, nil
+}
+
 func (c *StatusGoClient) getPeers() ([]Peer, error) {
 	var peers []Peer
 	err := c.rpcClient.Call(&peers, "admin_peers")
--- json.go
+++ json.go
@@ -2,9 +2,30 @@ package main
 
 import "fmt"
 
+type NodeInfo struct {
+	Enode       string                  `json:"enode"`
+	Name        string                  `json:"name"`
+	ID          PeerID                  `json:"id"`
+	ListenIp    string                  `json:"ip"`
+	ListenAddr  string                  `json:"listenAddr"`
+	ListenPorts NodeInfoPorts           `json:"ports"`
+	Protocols   map[string]NodeProtocol `json:"protocols"`
+}
+
+type NodeInfoPorts struct {
+	Discovery int `json:"discovert"`
+	Listener  int `json:"listener"`
+}
+
+type NodeProtocol struct {
+	MaxMessageSize int     `json:"maxMessageSize"`
+	MinimumPoW     float32 `json:"minimumPoW"`
+	Version        string  `json:"version"`
+}
+
 type Peer struct {
 	Enode     string            `json:"enode"`
-	ID        PeerId            `json:"id"`
+	ID        PeerID            `json:"id"`
 	Name      string            `json:"name"`
 	Caps      []string          `json:"caps"`
 	Network   NetworkInfo       `json:"network"`
@@ -15,10 +36,10 @@ func (p Peer) String() string {
 	return fmt.Sprintf("Peer(ID=%s)", p.ID)
 }
 
-type PeerId string
+type PeerID string
 
 // the ID is too long to display in full in most places
-func (id PeerId) String() string {
+func (id PeerID) String() string {
 	return fmt.Sprintf("%s...%s",
 		string(id[:6]),
 		string(id[len(id)-6:]))
