0baabc301ef8fb8e5700b3d28b9b0b858fe7f199
Jakub Sokołowski <jakub@status.im>
2019-07-03 15:25:22 -0400

    simplify rcp call methods
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- client.go
+++ client.go
@@ -16,38 +16,22 @@ func newClient(url string) (*StatusGoClient, error) {
 	return &StatusGoClient{rpcClient}, nil
 }
 
-func (c *StatusGoClient) nodeInfo() (*NodeInfo, error) {
-	var info NodeInfo
-	err := c.rpcClient.Call(&info, "admin_nodeInfo")
-	if err != nil {
-		return nil, err
-	}
-	return &info, nil
+func (c *StatusGoClient) nodeInfo() (info NodeInfo, err error) {
+	err = c.rpcClient.Call(&info, "admin_nodeInfo")
+	return
 }
 
-func (c *StatusGoClient) getPeers() ([]Peer, error) {
-	var peers []Peer
-	err := c.rpcClient.Call(&peers, "admin_peers")
-	if err != nil {
-		return nil, err
-	}
-	return peers, nil
+func (c *StatusGoClient) getPeers() (peers []Peer, err error) {
+	err = c.rpcClient.Call(&peers, "admin_peers")
+	return
 }
 
-func (c *StatusGoClient) removePeer(enode string) (bool, error) {
-	var rval bool
-	err := c.rpcClient.Call(&rval, "admin_removePeer", enode)
-	if err != nil {
-		return false, err
-	}
-	return rval, nil
+func (c *StatusGoClient) removePeer(enode string) (success bool, err error) {
+	err = c.rpcClient.Call(&success, "admin_removePeer", enode)
+	return
 }
 
-func (c *StatusGoClient) trustPeer(enode string) (bool, error) {
-	var rval bool
-	err := c.rpcClient.Call(&rval, "shh_markTrustedPeer", enode)
-	if err != nil {
-		return false, err
-	}
-	return rval, nil
+func (c *StatusGoClient) trustPeer(enode string) (success bool, err error) {
+	err = c.rpcClient.Call(&success, "shh_markTrustedPeer", enode)
+	return
 }
